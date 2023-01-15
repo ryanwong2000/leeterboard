cd198176fe82aae021a5c562c6106231b9283d11
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2023-01-14 23:31:42 -0500

    cleaned out old logic

--- leeterboard/src/App.tsx
+++ leeterboard/src/App.tsx
@@ -1,53 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { LeetCode } from 'leetcode-query';
 import type { RecentSubmission, User } from '../types/types';
 import UserCard from './components/UserCard';
-const lc = new LeetCode();
 
 function App() {
   const [userData, setUserData] = useState<User[]>([]);
 
   useEffect(() => {
+    console.log('useEffect');
     setUserData([
       {
         username: 'ryanwong2000',
-        streak: 2,
-        submittedToday: false
+        streak: 4,
+        submittedToday: false,
+        lastUpdated: new Date('2023-01-13'),
+        lastSubmitted: new Date('2023-01-14')
       }
     ]);
   }, []);
 
-  const getSubmittedToday = async (
-    username: string
-  ): Promise<RecentSubmission | undefined> => {
-    const submissions = await lc.recent_submissions(username);
-    return submissions.find(
-      (submission) =>
-        submission.statusDisplay === 'Accepted' &&
-        new Date(submission.timestamp).setHours(0, 0, 0, 0) ===
-          new Date().setHours(0, 0, 0, 0)
-    );
-  };
-
   const updateAllUsers = async () => {
     console.log('called update all users');
 
-    let temp = await Promise.all(
-      userData.map(async (user): Promise<User> => {
-        if (await getSubmittedToday(user.username)) {
-          user.submittedToday = true;
-          user.streak++;
-        } else {
-          user.submittedToday = false;
-          user.streak = 0;
-        }
-        console.log(user);
-
-        return user;
-      })
-    );
-    setUserData(temp);
+    const url = 'http://localhost:3005/getupdatedusers';
+    const res = await fetch(url);
+    const updatedUserData = await res.json();
+    setUserData(updatedUserData);
   };
 
   return (
