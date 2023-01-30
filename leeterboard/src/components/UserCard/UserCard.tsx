22d3bed1b541c4ebee13b9e7333c79e374b33787
ryanwong2000 <ryanwong2000@hotmail.com>
2023-01-29 20:54:53 -0500

    dark mode dummo

--- leeterboard/src/components/UserCard/UserCard.tsx
+++ leeterboard/src/components/UserCard/UserCard.tsx
@@ -5,8 +5,10 @@ export default function UserCard(props: { user: LCUser }) {
   return (
     <tr className="row">
       <td className="name">{props.user.username}</td>
+      <td className="submittedToday">
+        {props.user.submittedToday ? '✅' : '❌'}
+      </td>
       <td className="streak">{props.user.streak}</td>
-      <td className="submittedToday">{props.user.submittedToday ? '✅' : '❌'}</td>
       <td className="lastAccepted">{props.user.title}</td>
       <td className="language">{props.user.lang}</td>
     </tr>
